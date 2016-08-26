8b4c511ed746e13ea0d6b720530822692b2a9506
ntuifranklin <ntuifranklin2005@gmail.com>
2016-08-26 19:58:23 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-/* Index file
-*/
+/**
+  * This is index page.
+  *
+  *
+  * * Markdown style lists function too
+  * * Just try this out once
+  *
+  * The section after the description contains the tags; which provide
+  * structured meta-data concerning the given element.
+  *
+  * @author  Franklin Nkokam Ngongang <franklin.nkokam.ngongang@gmail.com>
+  *
+  * @since 1.0
+  *
+  * @param int    $example  This is an example function/method parameter description.
+  * @param string $example2 This is a second example.
+  */
 ?>
